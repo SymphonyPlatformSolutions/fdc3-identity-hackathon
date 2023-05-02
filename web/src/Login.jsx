958a0a612d173afaa1c0cda3b65fe72d0e5d5394
Antoine Rollin <antoine.rollin@symphony.com>
2023-05-02 11:40:02 +0200

    FE FDC3 GetIdentity

--- web/src/Login.jsx
+++ web/src/Login.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import "./Login.css";
 import { DesktopAgent } from '@finos/fdc3';
 
+const MOCK_JWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ0ZXN0IjoyfQ.dEHiqvpIWH7WDBumDsAcLoxf_CBVTIhPe0nwgag54Dp5H3NnHY_av0KyLh0pXyXF02TeNa_6v6Eb6sh6eHKv1EbWzd96btakFmoSQ3UYCIdsAq9OLj9xTbOVLvPUtdwsUPcUnCabTuUtGCwJzW1d6Sp9EBpL2KNZK2GhMwh29fEMsZmWOE2zydR8deujz-A3PFob4zeQgpP5EKQ5mKzwU7mvl9nStS7XqdcTJtztv5WRTyGDDuia3dO43nPTam61bdQL2nRE441i_tbiEuqnx4eom3CiTej0dusowTSVsl8m0t3m4kxjeDERpynhhZ842iigDY7GYjm62IC3riYA2g";
+
 const States = {
   FETCH_IDENTITY_SYMPHONY: 1,
   VALIDATE_IDENTITY: 2,
@@ -18,36 +20,18 @@ function Login({ onSuccess }) {
   const [state, setState] = useState();
   const [errorMessage, setErrorMessage] = useState();
 
-  useEffect(() => {
-    window.fdc3.addIntentListener('GetIdentityResponse', onGetIdentity);
-  }, [])
+  const getSymphonyIdentity = async () => {
+    try {
+      setState(States.FETCH_IDENTITY_SYMPHONY);
+      const resolution = await window.fdc3.raiseIntent("GetIdentity", {});
+      const result = await resolution.getResult();
+      return result.jwt;
+    } catch (error) {
+      setState(States.ERROR);
+      console.error(`${resolution.source} returned an error: ${error}`);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    raiseGetIdentityIntent();
-  }
-
-  const raiseGetIdentityIntent = () => {
-    setState(States.FETCH_IDENTITY_SYMPHONY);
-
-    window.fdc3.raiseIntent("GetIdentity", {});
-
-    // todo - remove
-    // mock response
-    setTimeout(() => {
-      onGetIdentity({ jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ0ZXN0IjoyfQ.dEHiqvpIWH7WDBumDsAcLoxf_CBVTIhPe0nwgag54Dp5H3NnHY_av0KyLh0pXyXF02TeNa_6v6Eb6sh6eHKv1EbWzd96btakFmoSQ3UYCIdsAq9OLj9xTbOVLvPUtdwsUPcUnCabTuUtGCwJzW1d6Sp9EBpL2KNZK2GhMwh29fEMsZmWOE2zydR8deujz-A3PFob4zeQgpP5EKQ5mKzwU7mvl9nStS7XqdcTJtztv5WRTyGDDuia3dO43nPTam61bdQL2nRE441i_tbiEuqnx4eom3CiTej0dusowTSVsl8m0t3m4kxjeDERpynhhZ842iigDY7GYjm62IC3riYA2g" });
-    }, 3000);
-  }
-
-  const onGetIdentity = async (payload) => {
-    const userInfo = await validateJwt(payload.jwt);
-
-    if (validatedJwt) {
-      setState(States.SUCCESS);
-
-      setTimeout(() => {
-        onSuccess(userInfo);
-      }, REDIRECTION_DELAY * 1000);
+      // TODO: remove once Sym is ready to handle GetIdentity
+      return MOCK_JWT;
     }
   }
 
@@ -66,10 +50,31 @@ function Login({ onSuccess }) {
         throw new Error(response.statusText);
       }
       return response.json();
-    }).catch((e) => {
-      setErrorMessage(e.toString());
+    }).catch((error) => {
       setState(States.ERROR);
+      setErrorMessage(error.toString());
     })
+  }
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const jwt = await getSymphonyIdentity();
+
+    if (!jwt) {
+      return;
+    }
+
+    const userContext = await validateJwt(jwt);
+
+    if (userContext) {
+      setState(States.SUCCESS);
+
+      setTimeout(() => {
+        onSuccess(userContext);
+      }, REDIRECTION_DELAY * 1000);
+    }
   }
 
   return (
