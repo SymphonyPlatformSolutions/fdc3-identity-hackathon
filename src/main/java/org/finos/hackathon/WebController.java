27f790f588e4f2d4f7e2c74a44856830f69b7ab2
Yong Sheng Tan <yong.tan@symphony.com>
2023-05-04 21:43:21 +0800

    Added transaction app

---
+++ src/main/java/org/finos/hackathon/WebController.java
@@ -0,0 +1,12 @@
+package org.finos.hackathon;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+    @RequestMapping(value = { "/", "/tx" })
+    public String getIndex() {
+        return "/index.html";
+    }
+}
